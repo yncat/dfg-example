9b792089e14bca02e1e760eed463460b1e1250a9
nyanchan <personal@nyanchangames.com>
2021-06-22 22:57:14 +0900

    Ask player count and their names

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,40 @@ async function inputFromUser(message: string): Promise<string> {
   });
 }
 
+async function inputNumberFromUser(
+  message: string,
+  min: number,
+  max: number
+): Promise<number> {
+  let ret: number;
+  while (true) {
+    const s = await inputFromUser(message + "(" + min + " - " + max + ")");
+    const n = parseInt(s);
+    if (isNaN(n)) {
+      console.log("数値を読み取れません。");
+      continue;
+    }
+    if (n < min || n > max) {
+      console.log("" + min + "から" + max + "の間で入力してください。");
+      continue;
+    }
+    ret = n;
+    break;
+  }
+  return ret;
+}
+
 async function main(): Promise<void> {
-  const a = await inputFromUser("test");
-  console.log(a);
+  const numPlayers = await inputNumberFromUser("プレイヤーの人数", 2, 20);
+  console.log(numPlayers);
+  const playerNames: string[] = [];
+  for (let i = 0; i < numPlayers; i++) {
+    const pn = i + 1;
+    const pname = await inputFromUser(
+      "プレイヤー" + pn + "の名前(省略時は p" + pn + ")"
+    );
+    playerNames.push(pname == "" ? "p" + pn : pname);
+  }
 }
 
 const rl = readline.createInterface({
